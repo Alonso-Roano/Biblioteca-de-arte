fa0c61324f6e99522983fbc2790c7f1b3f7cc5d6
SirMauricio <190300383@unicaribe.edu.mx>
2025-03-18 01:24:43 -0500

    Agragar DetalleProducto y cambios en footer

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,6 +10,7 @@ import DashboardEtiquetas from '@/views/Dashboard/DashboardEtiquetas.vue'
 import DashboardLikes from '@/views/Dashboard/DashboardLikes.vue'
 import DashboardProfile from '@/views/Dashboard/DashboardProfile.vue'
 import ErrorPage404 from '@/views/Errors/ErrorPage404.vue'
+import DetalleProducto from '../views/DetalleProducto.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -25,6 +26,12 @@ const router = createRouter({
       name: 'login',
       meta: { menu: true, title: 'Login' },
       component: LoginView
+    },
+    {
+      path: '/DetalleProducto',
+      name: 'Detalle de producto',
+      meta: { menu: true, title: 'Detalle de producto' },
+      component: DetalleProducto
     },
     {
       path: '/register',
